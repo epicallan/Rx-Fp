1f467c1e9760db7bebc32d7e79bb63060cc51449
allan <epicallan.al@gmail.com>
2016-06-07 01:14:46 +0300

    map composition error

--- src/app.js
+++ src/app.js
@@ -2,17 +2,24 @@ import axios from 'axios';
 import Rx from 'rxjs/Rx';
 import _ from 'ramda';
 
-// get data from flickr api
-const url = term =>
-  (`https://api.flickr.com/services/feeds/photos_public.gne?tags= ${term}&format=json&jsoncallback=?`);
+// get data from github
+const url = user =>
+  (`https://api.github.com/users/${user}/repos`);
 
+const setHtml = _.curry((sel, html) => (document.getElementById(sel).innerHTML = html));
 
-export const app = _.compose(axios.get, url);
+export const repoNames = (name) => (`<li>${name}</li>`);
 
-export const source = Rx.Observable.fromPromise(app('cats'));
+export const items = _.compose(_.map(repoNames), _.prop('name'));
 
-// source.subscribe(
-//   src => { console.log('Next', src); },
-//   err => { console.log('error', err); },
-//   () => { console.log('completed'); }
-// );
+export const renderNames = _.compose(setHtml('root'), items);
+
+export const getData = _.compose(axios.get, url);
+
+export const app = Rx.Observable.fromPromise(getData('epicallan'));
+
+app
+  .map(response => response.data)
+  .subscribe(
+    data => renderNames(data)
+  );
--- src/tests/app-test.js
+++ src/tests/app-test.js
@@ -1,16 +1,25 @@
 import { expect } from 'chai';
-import { source, app } from '../app.js';
+import data from './data.json';
+import _ from 'ramda';
+import { repoNames, app } from '../app.js';
 
-describe('tests for flickr app', () => {
-  it('App should be a promise', () => {
-    expect(app('cats')).to.be.a('Promise');
+describe('tests for app', () => {
+  it('renderNames should render repo names as list items', () => {
+    // const listOfNames = renderNames(data);
+    // console.log(listOfNames[0]);
+    const names = _.compose(_.map(repoNames), _.prop('name'));
+    // const nameItems = _.map(_.prop('name'));
+    const listOfNames = names(data);
+    console.log(listOfNames);
+    expect(listOfNames).to.be.an('array');
   });
   /* eslint-disable func-names */
-  it('should return observer with images from flickr', function (done) {
+  it('should return an observerable with github data', function (done) {
     this.timeout(5000);
-    source.subscribe(
+    app
+    .subscribe(
       response => {
-        expect(response.data).to.be.a('string');
+        expect(response.data).to.be.a('array');
       },
       err => { console.log('error', err); },
       () => {
